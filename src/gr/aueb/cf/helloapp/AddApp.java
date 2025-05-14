f76e0d762aa5a301ecd00f29f56b2602d64e9146
liavergou <liaverg@gmail.com>
2025-05-14 20:36:01 +0300

    fix: resolve merge conflict between main and fix/addapp-print-formated

--- src/gr/aueb/cf/helloapp/AddApp.java
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -4,11 +4,8 @@ public class AddApp {
 
     public static void main(String[] args) {
         System.out.println(add(3,5));
-<<<<<<< HEAD
+
         System.out.printf("%4d", add(7,2));
-=======
-        System.out.printf("%d", add(3,5));
->>>>>>> fix/addapp-print-formated
 
     }
     public static int add(int a, int b){
